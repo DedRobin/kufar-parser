da7eb9ad447c0cf58232a96449545d3273a03012
Pavel Rabekin <pavelrabekin@gmail.com>
2023-02-02 16:05:07 +0300

    fixed

--- services.py
+++ services.py
@@ -1,5 +1,10 @@
 import inspect
 import logging
+import shutil
+import urllib
+import pathlib
+import requests
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 from parsers.kufar_parser import parse_kufar
@@ -13,7 +18,7 @@ logging.basicConfig(
 reply_keyboard = [
     ["Получить объявления"],
 ]
-markup = ReplyKeyboardMarkup(reply_keyboard)
+markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -35,11 +40,24 @@ async def get_updates(update: Update, context: ContextTypes.DEFAULT_TYPE):
     command = inspect.currentframe().f_code.co_name
 
     logger.info("{0} {1} used command '/{2}'".format(first_name, last_name, command))
-    products = parse_kufar()
 
-    for product in products:
+    # products = parse_kufar()
+    products = []
+
+    # for product in products:
+    for _ in range(1):
+        # await context.bot.send_message(chat_id=update.effective_chat.id,
+        #                                text=product[0])
+        url = 'https://yams.kufar.by/api/v1/kufar-ads/images/88/8823024759.jpg?rule=list_thumbs_2x'
+        res = requests.get(url, stream=True)
+        with open("test.png", "wb") as f:
+            shutil.copyfileobj(res.raw, f)
         await context.bot.send_photo(
             chat_id=update.effective_chat.id,
-            photo=product[-1],
-            caption="\n".join(product[:-1])
+            # photo=product[-1],
+            photo='https://yams.kufar.by/api/v1/kufar-ads/images/88/8823024759.jpg?rule=list_thumbs_2x',
+            # photo=pathlib.Path("test.png"),
+
+            # caption="\n".join(product[:-1]),
         )
+        # await asyncio.sleep(0.5)
