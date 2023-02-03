10fed2c556f1cbc8e821b347ff6596222ffbb64d
Pavel Rabekin <pavelrabekin@gmail.com>
2023-02-03 16:05:54 +0300

    parser can go to next page

--- parsers/kufar_parser.py
+++ parsers/kufar_parser.py
@@ -1,4 +1,3 @@
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -8,49 +7,72 @@ URL = "https://www.kufar.by/l/r~minsk/noutbuki/nb~apple?cmp=0&cnd=1&sort=lst.d"
 
 
 def parse_kufar(url: str = URL) -> list:
+    products = []
+
     # Options
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--window-size=1920,1080')
 
     # Open browser
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path="driver/chromedriver", options=chrome_options)
     driver.get(url=url)
-    class_name = "styles_wrapper__IMYdY"
-    products = driver.find_elements(By.TAG_NAME, "section")  # [:5]
 
-    names, prices, links, images, dates = [], [], [], [], []
+    # Click button "Принять"
+    # driver.find_element(By.XPATH, '//*[@id="__next"]/div[3]/div/div[2]/button').click()
 
-    for num, p in enumerate(products, 1):
-        name = p.find_element(By.CLASS_NAME, "styles_title__XS_QS")
-        name = name.text
-        names.append(name)
+    # Signal
+    signal = True
 
-        price = p.find_element(By.CLASS_NAME,
-                               'styles_price__tiO8k')
-        price = price.find_element(By.TAG_NAME, "span")
-        price = price.text
-        prices.append(price)
+    while signal:
+        products_from_page = driver.find_elements(By.TAG_NAME, "section")  # [:5]
 
-        link = p.find_element(By.CLASS_NAME, 'styles_wrapper__IMYdY')
-        link = link.get_attribute("href")
-        links.append(link)
+        names, prices, links, images, dates = [], [], [], [], []
 
+        for num, p in enumerate(products_from_page, 1):
+            name = p.find_element(By.CLASS_NAME, "styles_title__XS_QS")
+            name = name.text
+            names.append(name)
+
+            price = p.find_element(By.CLASS_NAME,
+                                   'styles_price__tiO8k')
+            price = price.find_element(By.TAG_NAME, "span")
+            price = price.text
+            prices.append(price)
+
+            link = p.find_element(By.CLASS_NAME, 'styles_wrapper__IMYdY')
+            link = link.get_attribute("href")
+            links.append(link)
+
+            try:
+                image = p.find_element(By.CLASS_NAME, "styles_image__CTXvl")
+                src = image.get_attribute("data-src")
+            except NoSuchElementException:
+                src = "static/images/no-image.jpg"
+
+            images.append(src)
+
+            date = p.find_element(By.CLASS_NAME, 'styles_secondary__dylmH')
+            date = date.find_element(By.TAG_NAME, "span")
+            date = date.text
+            dates.append(date)
+
+        product_list = list(zip(links, names, prices, dates, images))
+        products.append(product_list)
+
+        # Get next page
+        next_page = driver.find_elements(By.CLASS_NAME, 'styles_link__KajLs.styles_arrow__fJMcy')
+
+        last_element = next_page[-1]
+        if last_element.tag_name != "a":
+            break
+        next_url = last_element.get_attribute("href")
         try:
-            image = p.find_element(By.CLASS_NAME, "styles_image__CTXvl")
-            src = image.get_attribute("data-src")
-        except NoSuchElementException:
-            src = "static/images/no-image.jpg"
+            driver.get(url=next_url)
+        except Exception as _ex:
+            print(_ex)
 
-        images.append(src)
-
-        date = p.find_element(By.CLASS_NAME, 'styles_secondary__dylmH')
-        date = date.find_element(By.TAG_NAME, "span")
-        date = date.text
-        dates.append(date)
-
-    data = list(zip(links, names, prices, dates, images))
-
+    # Closing browser
     driver.close()
     driver.quit()
-    return data
+    return products
--- services.py
+++ services.py
@@ -1,11 +1,7 @@
 import inspect
 import logging
-import shutil
-import urllib
-import pathlib
-import requests
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 from parsers.kufar_parser import parse_kufar
 
@@ -22,16 +18,12 @@ markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # keyboard = [
-    #     [InlineKeyboardButton("Получить последние объявления", callback_data='1'),
-    #      ]
-    # ]
     first_name = update.effective_chat.first_name
     last_name = update.effective_chat.last_name
     command = inspect.currentframe().f_code.co_name
 
     logger.info("{0} {1} used command '/{2}'".format(first_name, last_name, command))
-    await update.message.reply_text('Выберите действие:', reply_markup=markup)
+    await update.message.reply_text('Бот запущен.', reply_markup=markup)
 
 
 async def get_updates(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -41,23 +33,13 @@ async def get_updates(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     logger.info("{0} {1} used command '/{2}'".format(first_name, last_name, command))
 
-    # products = parse_kufar()
-    products = []
+    pages = parse_kufar()
+    # url = 'https://yams.kufar.by/api/v1/kufar-ads/images/88/8823024759.jpg?rule=list_thumbs_2x'
 
-    # for product in products:
-    for _ in range(1):
-        # await context.bot.send_message(chat_id=update.effective_chat.id,
-        #                                text=product[0])
-        url = 'https://yams.kufar.by/api/v1/kufar-ads/images/88/8823024759.jpg?rule=list_thumbs_2x'
-        res = requests.get(url, stream=True)
-        with open("test.png", "wb") as f:
-            shutil.copyfileobj(res.raw, f)
-        await context.bot.send_photo(
-            chat_id=update.effective_chat.id,
-            # photo=product[-1],
-            photo='https://yams.kufar.by/api/v1/kufar-ads/images/88/8823024759.jpg?rule=list_thumbs_2x',
-            # photo=pathlib.Path("test.png"),
-
-            # caption="\n".join(product[:-1]),
-        )
-        # await asyncio.sleep(0.5)
+    for page in pages:
+        for product in page:
+            await context.bot.send_photo(
+                chat_id=update.effective_chat.id,
+                photo=product[-1],
+                caption="\n".join(product[:-1]),
+            )
